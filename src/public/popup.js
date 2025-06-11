3b75b1df600be1cbc625148fb48561da3b3db260
JonesBryan <jones.bryan171@gmail.com>
2025-06-11 03:48:41 -0400

    better popup page

--- src/public/popup.js
+++ src/public/popup.js
@@ -1,12 +1,14 @@
-document
-  .getElementById("tokenForm")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
-    const token = document.getElementById("token").value;
+document.getElementById("tokenForm").addEventListener("submit", (event) => {
+  event.preventDefault();
+  const token = document.getElementById("token").value.trim();
 
-    // Save the token to local storage
-    chrome.storage.local.set({ anilistToken: token }, function () {
-      console.log("Token saved:", token);
-      alert("Token saved successfully!");
-    });
+  if (!token) {
+    alert("Please enter a token.");
+    return;
+  }
+
+  chrome.storage.local.set({ anilistToken: token }, () => {
+    console.log("Token saved:", token);
+    alert("Token saved successfully!");
   });
+});
